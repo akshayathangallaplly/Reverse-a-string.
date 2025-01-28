4d3b31cf4f84cf00cab56673aa668883c9ea75ea
akshayathangallapally <ta7440360@gmail.com>
2025-01-28 23:32:13 +0530

    Add files via upload

---
+++ Reversestr.java
@@ -0,0 +1,10 @@
+public class Reversestr {
+    public static void main(String[] args) {
+        String str = "varsha";
+
+        String reversed = new StringBuilder(str).reverse().toString();
+
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + reversed);
+    }
+}
